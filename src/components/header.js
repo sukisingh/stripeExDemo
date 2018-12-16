d2079d341ce88e793d71e1930555f2557af9e173
suki <sukisingh02@gmail.com>
2018-12-16 08:59:53 +0530

    header

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,21 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import './Header.css'
+
+//import logo from '../images/logo1.png'
+
 const Header = ({ siteTitle }) => (
-  <div>
-    Header
+  <div className="Header">
+    <div className="HeaderGroup">
+      <Link to="/"><img src={require('../images/logo1.png')} width="35"/></Link>
+      <Link to="/products">Products</Link>
+      <Link to="/developers">Developers</Link>
+      <Link to="/company">Company</Link>
+      <Link to="/pricing">Pricing</Link>
+      <Link to="/signin">Sign In</Link>
+    </div>
+   
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <div class="main">
+
+        {/*stripes*/}
         <div id="stripes">
           <span></span>
           <span></span>
@@ -16,16 +18,42 @@ const IndexPage = () => (
           <span></span>
           <span></span>
         </div>
+        {/*Header title */}
         <div className="Hero">
           <div class="HeroGroup">
               <h1>Lorem ipsum dolor consectetur adipiscing elit</h1>
               <p>Maecenas venenatis volutpat nibh vitae blandit. Morbi at diam neque. Donec vel lorem est. Nunc sodales porta est, nec vulputate dui accumsan at. </p>
-              <Link to="/page-2/">Create Account</Link> 
-             
+              <div className="heroButton">
+                  <Link to="/page-2/"><button class="hero-create-btn">Create Account</button></Link>
+                 <Link to="/page-2/"><button class="hero-contact-btn">Contact Sales</button></Link>
+              </div>
           </div>
         </div>
+
+        {/*App images*/}
+        {/*     <section id="app-illustrations" class="hidden-preload">
+               <div class="tablet-landscape">
+                  <img alt="shopify" src="/img/v3/home/app-illustrations/shopify.svg">
+               </div>
+               <div class="phone-big">
+                  <img alt="digitalocean" src="/img/v3/home/app-illustrations/digitalocean.svg">
+               </div>
+               <div class="phone-small">
+                  <img alt="postmates" src="/img/v3/home/app-illustrations/postmates.svg">
+               </div>
+               <div class="tablet-portrait">
+                  <img alt="asana" src="/img/v3/home/app-illustrations/asana.svg">
+               </div>
+            </section> */}
+        <div id="app-images">
+            <div class="tab-ls"></div>
+            <div class="p-big"></div>
+            <div class="p-small"></div>
+            <div class="tab-p"></div>
+        </div>
+        
       
-    </div>
+    </div>  {/*main class ends */}
   </Layout>
 )
 
